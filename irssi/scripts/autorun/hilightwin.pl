2c432ae404ba1878dc5d1774d522bac934e387d9
Derrick R. Burns <burns.derrick@gmail.com>
2013-05-06 04:05:21 -0400

    diff'ed in some upstream updates

--- irssi/scripts/autorun/hilightwin.pl
+++ irssi/scripts/autorun/hilightwin.pl
@@ -1,16 +1,18 @@
 # Print hilighted messages & private messages to window named "hilight"
 # for irssi 0.7.99 by Timo Sirainen
 use Irssi;
-use vars qw($VERSION %IRSSI); 
-$VERSION = "0.01";
+use POSIX;
+use vars qw($VERSION %IRSSI);
+
+$VERSION = "0.02";
 %IRSSI = (
-    authors	=> "Timo \'cras\' Sirainen",
-    contact	=> "tss\@iki.fi", 
-    name	=> "hilightwin",
-    description	=> "Print hilighted messages & private messages to window named \"hilight\"",
-    license	=> "Public Domain",
-    url		=> "http://irssi.org/",
-    changed	=> "2002-03-04T22:47+0100"
+    authors     => "Timo \'cras\' Sirainen, Mark \'znx\' Sangster",
+    contact     => "tss\@iki.fi, znxster\@gmail.com",
+    name        => "hilightwin",
+    description => "Print hilighted messages to window named \"hilight\"",
+    license     => "Public Domain",
+    url         => "http://irssi.org/",
+    changed     => "Sun May 25 18:59:57 BST 2008"
 );
 
 sub sig_printtext {
@@ -24,6 +26,24 @@ sub sig_printtext {
 
     if ($dest->{level} & MSGLEVEL_PUBLIC) {
       $text = $dest->{target}.": ".$text;
+    if(Irssi::settings_get_bool('hilightwin_showprivmsg')) {
+        $opt = MSGLEVEL_HILIGHT|MSGLEVEL_MSGS;
+    }
+
+    if(
+        ($dest->{level} & ($opt)) &&
+        ($dest->{level} & MSGLEVEL_NOHILIGHT) == 0
+    ) {
+        $window = Irssi::window_find_name('hilight');
+
+        if ($dest->{level} & MSGLEVEL_PUBLIC) {
+            $text = $dest->{target}.": ".$text;
+        }
+        $text = strftime(
+            Irssi::settings_get_str('timestamp_format')." ",
+            localtime
+        ).$text;
+        $window->print($text, MSGLEVEL_NEVER) if ($window);
     }
     $text =~ s/%/%%/g;
     $window->print($text, MSGLEVEL_CLIENTCRAP) if ($window);
